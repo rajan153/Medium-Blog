173b3c56b2268020eb8c18b1819277aef2e4f31c
Rajan <rajanmasih154@gmail.com>
2024-08-10 00:48:54 +0530

    feat: Add CORS middleware for API routes

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { userRoutes } from "./routes/user";
 import { blogRoutes } from "./routes/blogs";
+import { cors } from "hono/cors";
 
 const app = new Hono<{
   Bindings: {
@@ -9,6 +10,18 @@ const app = new Hono<{
   };
 }>();
 
+app.use("/api/*", cors());
+app.use(
+  "/api/*",
+  cors({
+    origin: "*",
+    allowHeaders: ["X-Custom-Header", "Upgrade-Insecure-Requests"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+    maxAge: 600,
+    credentials: true,
+  })
+);
 
 app.route("/api/v1/users", userRoutes);
 app.route("/api/v1/blog", blogRoutes);
