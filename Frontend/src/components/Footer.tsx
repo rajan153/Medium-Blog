efa6b242b00800b91ebead1d71d7083a832030f6
Rajan <rajanmasih154@gmail.com>
2024-09-17 02:57:15 +0530

    chore: Footer component

---
+++ Frontend/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+export function Footer() {
+  return (
+    <footer className="p-4 border-t-[1px] border-black">
+      <ul className="flex gap-4 justify-center items-center text-[#6B6B6B] flex-wrap text-sm">
+        <li>Help</li>
+        <li>Status</li>
+        <li>About</li>
+        <li>Careers</li>
+        <li>Press</li>
+        <li>Blog</li>
+        <li>Privacy</li>
+        <li>Terms</li>
+        <li>Text to speech</li>
+        <li>Terms</li>
+      </ul>
+    </footer>
+  );
+}
